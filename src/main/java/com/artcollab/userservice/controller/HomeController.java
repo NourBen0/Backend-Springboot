410ea9a0940a6adb14d24b312d4d47c1cc85506e
Nour Ben Brahim <107277249+NourBen0@users.noreply.github.com>
2025-09-06 19:34:58 +0100

    jjj
    hhkkk

---
+++ src/main/java/com/artcollab/userservice/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.artcollab.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "User microservice for salon booking system";
+    }
+}
